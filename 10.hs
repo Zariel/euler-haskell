972f7fb0bfa73cd3333475d9f708c6d392830e5f
Chris Bannister <c.bannister@gmail.com>
2010-01-09 13:12:44 +0000

    Use floats

--- 10.hs
+++ 10.hs
@@ -1,4 +1,4 @@
-primes :: Int -> [Int]
-primes limit = 2 : [ p | p <- [ 3, 5 .. limit ], isPrime p ]
+primes :: Float -> [ Float ]
+primes limit = (2.0 : [ p | p <- [ 3.0, 5.0 .. limit ], isPrime (p) ])
 	where
-	isPrime x = not (any (\d -> x `mod` d == 0) (2 : [ 3, 5 .. ceiling (sqrt (fromIntegral x)) ]))
+	isPrime x = not ((any (\d -> fromEnum (x) `mod` d == 0)) ([ 3, 5 .. fromEnum (sqrt (x)) ]))
